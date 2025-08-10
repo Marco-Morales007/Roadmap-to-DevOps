83b81bc9cef07152f91dddb75bb7ab2e3a02514a
Marco <123842616+Marco-Morales007@users.noreply.github.com>
2025-08-10 00:32:09 -0600

    Delete extra directory

--- nivel_2-Automatizacion_red/nivel_1-Fundamentos/ejercicio1_int-out.py
+++
@@ -1,5 +0,0 @@
-#crear un programa basico que pida el nombre y devuelva un saludo personalizado
-
-nombre = input("Nombre: ")
-apellido = input("Apellido: ")
-print(f"Su nombre completo es: {nombre} con apellido {apellido}")
